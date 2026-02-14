cf06f86b57a39d523dd4017ef87a610340d62b15
ogash3103 <114906301+ogash3103@users.noreply.github.com>
2026-02-14 21:20:26 +0500

    n songacha bo’lgan sonlarni yozuv shaklida chiqaruvchi generator hosil qiling: 1 => “bir”

--- bin/future_stream.dart
+++ bin/future_stream.dart
@@ -25,7 +25,35 @@ Future<void> main() async {
   for(final item in specialMultiplesGenerator(n, m, k)) {
     print(item);
   }
+
+  int nS = 10;
+  for(final item in numberWordsUpTo(nS)) {
+    print(item);
+  }
 }
+Iterable<String> numberWordsUpTo(int n) sync* {
+  for(int i = 1; i <= n; i++) {
+    yield "$i => ${numberToWord(i)}";
+  }
+}
+
+
+String numberToWord(int n) {
+  switch (n) {
+    case 1: return "bir";
+    case 2: return "ikki";
+    case 3: return "uch";
+    case 4: return "to‘rt";
+    case 5: return "besh";
+    case 6: return "olti";
+    case 7: return "yetti";
+    case 8: return "sakkiz";
+    case 9: return "to‘qqiz";
+    case 10: return "o‘n";
+    default: return "noma’lum";
+  }
+}
+
 
 Iterable<int> multiplesOf7UpTo(int n) sync* {
   for(int i = 7; i <= n; i += 7) {
