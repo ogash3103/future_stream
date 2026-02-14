70d8c3ac8bc18e83b05985292ce73b2e4952e01b
ogash3103 <114906301+ogash3103@users.noreply.github.com>
2026-02-14 21:16:05 +0500

    Kiritilgan n songacha bo’lgan quyidagi generatorlarni hosil qiling: 1. n songacha bo’lgan faqat 7 ga karrali sonlarni chiqaruvchi generator. 2. n songacha bo’lgan faqat m ga karrali sonlarni chiqaruvchi va agar m soniga karralilar orasida k soniga karralisi bo’lsa o’sha sonni k ga bo’lib chiqaruvchi generator hosil qiling.

--- bin/future_stream.dart
+++ bin/future_stream.dart
@@ -1,20 +1,51 @@
 Future<void> main() async {
 
-  final value = await addTwoNumber(3, 4);
-  print(value);
+  // final value = await addTwoNumber(3, 4);
+  // print(value);
+  //
+  //
+  // print("Starting long operation…");
+  //
+  // await Future.delayed(Duration(seconds: 5));
+  //
+  // print("Long operation complete!");
 
+  int value = 50;
 
-  print("Starting long operation…");
+  for(final item in multiplesOf7UpTo(value)) {
+    print(item);
+  }
 
-  await Future.delayed(Duration(seconds: 5));
+  print('=========');
 
-  print("Long operation complete!");
+  int n = 100;
+  int m = 10;
+  int k = 20;
 
+  for(final item in specialMultiplesGenerator(n, m, k)) {
+    print(item);
+  }
 }
 
-Future<int> addTwoNumber(int a, int b) async{
-
-  await Future.delayed(Duration(seconds: 3));
-
-  return a + b;
+Iterable<int> multiplesOf7UpTo(int n) sync* {
+  for(int i = 7; i <= n; i += 7) {
+    yield i;
+  }
 }
+
+Iterable<int> specialMultiplesGenerator(int n, int m, int k) sync*{
+  for(int i = m; i <= n; i += m) {
+    if(i % k == 0) {
+      yield i ~/ k;
+    } else {
+      yield i;
+    }
+  }
+}
+
+// Future<int> addTwoNumber(int a, int b) async{
+//
+//   await Future.delayed(Duration(seconds: 3));
+//
+//   return a + b;
+// }
