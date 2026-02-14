c793a7b463f26471656ac06a91307e853e0ac180
ogash3103 <114906301+ogash3103@users.noreply.github.com>
2026-02-14 20:28:44 +0500

    Exercise • Async va await ni ishlatgan holda 2 son qabul qilib ularni yig’indisini 3 soniyadan so’ng qaytarib beruvchi dastur tuzing. • Async va await ishlatgan holda long-running operatsiyasini ko’rsatuvchi dastur tuzing. Dastur "Starting long operation…" print qilishi kerak va 5 soniyadan so’ng "Long operation complete!" Print qilinadi.
    DONE

--- bin/future_stream.dart
+++ bin/future_stream.dart
@@ -2,7 +2,8 @@ Future<void> main() async {
 
   final value = await addTwoNumber(3, 4);
   print(value);
-  
+
+
   print("Starting long operation…");
 
   await Future.delayed(Duration(seconds: 5));
