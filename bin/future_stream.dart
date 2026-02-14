a17610bd5600d97b73ac17b4917a72c7a4942b90
ogash3103 <114906301+ogash3103@users.noreply.github.com>
2026-02-14 20:27:55 +0500

    Initial commit

---
+++ bin/future_stream.dart
@@ -0,0 +1,19 @@
+Future<void> main() async {
+
+  final value = await addTwoNumber(3, 4);
+  print(value);
+  
+  print("Starting long operation…");
+
+  await Future.delayed(Duration(seconds: 5));
+
+  print("Long operation complete!");
+
+}
+
+Future<int> addTwoNumber(int a, int b) async{
+
+  await Future.delayed(Duration(seconds: 3));
+
+  return a + b;
+}
